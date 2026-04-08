0b53cb6843d6d4945a44e1a93e0e48ec76d6d02d
Fejiro001 <aberefejiro@gmail.com>
2026-04-08 18:04:43 -0500

    Added background music on game start and cleaned up end game function

--- assets/script/app.js
+++ assets/script/app.js
@@ -8,12 +8,103 @@ const currentPoints = document.querySelector(".points-value");
 const timeCountdown = document.querySelector(".time-value");
 const inputField = document.querySelector(".word-input");
 
-const wordsArray = ["dino", "love"];
+const wordsArray = [
+  "dinosaur",
+  "love",
+  "pineapple",
+  "calendar",
+  "robot",
+  "building",
+  "population",
+  "weather",
+  "bottle",
+  "history",
+  "dream",
+  "character",
+  "money",
+  "absolute",
+  "discipline",
+  "machine",
+  "accurate",
+  "connection",
+  "rainbow",
+  "bicycle",
+  "eclipse",
+  "calculator",
+  "trouble",
+  "watermelon",
+  "developer",
+  "philosophy",
+  "database",
+  "periodic",
+  "capitalism",
+  "abominable",
+  "component",
+  "future",
+  "pasta",
+  "microwave",
+  "jungle",
+  "wallet",
+  "canada",
+  "coffee",
+  "beauty",
+  "agency",
+  "chocolate",
+  "eleven",
+  "technology",
+  "alphabet",
+  "knowledge",
+  "magician",
+  "professor",
+  "triangle",
+  "earthquake",
+  "baseball",
+  "beyond",
+  "evolution",
+  "banana",
+  "perfumer",
+  "computer",
+  "management",
+  "discovery",
+  "ambition",
+  "music",
+  "eagle",
+  "crown",
+  "chess",
+  "laptop",
+  "bedroom",
+  "delivery",
+  "enemy",
+  "button",
+  "superman",
+  "library",
+  "unboxing",
+  "bookstore",
+  "language",
+  "homework",
+  "fantastic",
+  "economy",
+  "interview",
+  "awesome",
+  "challenge",
+  "science",
+  "mystery",
+  "famous",
+  "league",
+  "memory",
+  "leather",
+  "planet",
+  "software",
+  "update",
+  "yellow",
+  "keyboard",
+  "window"
+];
 const backgroundMusic = new Audio("./assets/media/audio/background.wav");
 backgroundMusic.type = "audio/wav";
 let currentIndex = 0;
 let gamePoints = 0;
-let timeLeft = 5;
+let timeLeft = 99;
 let timer;
 const scoresArray = [];
 
@@ -83,8 +174,6 @@ const checkAllMatched = () => {
     clearInput();
     incrementPoints();
     showNextWord();
-    // if (currentIndex < wordsArray.length) {
-    // }
   }
 };
 
@@ -109,6 +198,8 @@ inputField.addEventListener("input", (e) => {
 
 // Start game
 const startGame = () => {
+  backgroundMusic.play();
+  backgroundMusic.loop = true;
   shuffle(wordsArray);
   showNextWord();
   startCountdown();
@@ -117,8 +208,7 @@ const startGame = () => {
 startGame();
 
 const createNewScoreObject = () => {
-  const date = new Date();
-  date.toLocaleDateString("en-ca", {
+  const date = new Date().toLocaleDateString("en-ca", {
     day: "numeric",
     month: "long",
     year: "numeric"
@@ -131,6 +221,13 @@ const createNewScoreObject = () => {
 };
 
 const endGame = () => {
+  // Stop timer
+  clearInterval(timer);
+
+  // Stop music
+  backgroundMusic.pause();
+
+  // Create and store score
   const scoreObj = createNewScoreObject();
   scoresArray.push(scoreObj);
 };
