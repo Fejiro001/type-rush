4481a360def190c62a74957aead6c04b4197b03b
Fejiro001 <aberefejiro@gmail.com>
2026-04-08 16:39:21 -0500

    Created the words array shuffle function

--- assets/script/app.js
+++ assets/script/app.js
@@ -93,3 +93,14 @@ const wordsArray = [
   "keyboard",
   "window"
 ];
+
+// Shuffle function
+const shuffle = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+
